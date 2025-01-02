37d9db4d703bfae2b3c972db42f35d46a80767b3
Shubham Borse <shubhamborse0911@gmail.com>
2025-01-02 20:32:41 +0530

    Child to parent communication

---
+++ ChildToParent/src/app/child/child.component.ts
@@ -0,0 +1,20 @@
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+
+  @Output() public LaserLight = new EventEmitter();
+
+  public SendMessage()
+  {
+    this.LaserLight.emit("Hello Parent...");
+  }
+
+}
